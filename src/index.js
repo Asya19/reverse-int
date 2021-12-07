49896b466ce7058dcef6d0c90074aec9df64adc1
Asya19 <58248621+Asya19@users.noreply.github.com>
2021-12-07 23:53:31 +0300

    Update index.js
    feat: add code to implement the function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+    "use strict";
+    let reverseNum = 0;
+    let tmp = n;
+    if (tmp < 0) {
+        tmp = tmp * (-1);
+    }
+    while (tmp > 0) {
+        reverseNum = reverseNum * 10 + tmp % 10;
+        tmp = parseInt(tmp / 10);
+    }
+    return reverseNum;
 }
